be22b4ab5a6588bbe95022e57e5305c7ddc7f501
robie sun <robie.sun.rs@gmail.com>
2017-10-22 23:44:50 +0200

    Dependency injection FormBuilder added and Validators are also added

--- src/app/videos/video-create/video-create.component.ts
+++ src/app/videos/video-create/video-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -12,11 +12,12 @@ export class VideoCreateComponent implements OnInit {
 
   videoGroup: FormGroup;
 
-  constructor(private router: Router) {
-    this.videoGroup = new FormGroup({
-      title: new FormControl(),
-      about: new FormControl()
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.videoGroup = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(4)]],
+      about: ['', Validators.required]
     });
+
   }
 
   ngOnInit() {
